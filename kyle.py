b9a5b43e9d4a24b4df3d072431f839ba79d15383
“kyleisme14” <kylekallman@gmail.com>
2022-01-14 16:07:11 -0800

    food pricer

--- kyle.py
+++ kyle.py
@@ -1,5 +1,9 @@
 # getting specific titles from like a list of books?
 # going through a list of books to get all mtheir names?
+#instead. pricing through books.
+
+import re
+
 
 bookPrice = [11, 22, 33 ,44]
 HarryPotter = 11
@@ -31,3 +35,25 @@ def bookpricer(book):
 
 bookpricer(MobyDick)
 
+
+## menu items price increase    
+
+sandwich = 1
+soda = 2
+chips = 3
+salad = 4
+food = [1,2,3,4]
+
+def foodPriceAdjuster(nums):
+    return nums + .99
+
+results2 = map(foodPriceAdjuster, food)
+print("new price = ", list(results2))
+
+def foodPricer(nums):
+    if nums > 2:
+        print("YUP")
+    else:
+        print("NO WAY")
+
+foodPricer(salad)
